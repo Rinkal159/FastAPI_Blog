52937c56353691ac0fe39bd5e18665392f3542af
rinkal159 <rinkalsingapuri@gmail.com>
2026-07-03 19:15:29 +0530

    Builds BlogCreate class.

--- schemas/blog_schema.py
+++ schemas/blog_schema.py
@@ -2,12 +2,28 @@ from pydantic import BaseModel, Field
 from typing import Annotated
 from datetime import datetime
 
+
 class Base(BaseModel):
     pass
 
-class BlogResponse(Base):
-    id: int
-    title: str
-    content: str
-    author: str
+
+class BlogCreate(Base):
+    title: Annotated[str, Field(min_length=1, max_length=100)]
+    content: Annotated[str, Field(min_length=10, max_length=1500)]
+
+
+class AuthorDetail(Base):
+    name: str
+    profile_picture: str | None
+    profile_picture_path: str
+    bio: str | None
+
+
+# inheritated from BlogCreate
+class BlogResponse(BlogCreate):
     posted_at: datetime
+    updated_at: datetime
+    author: AuthorDetail
+
+    class Config:
+        orm_mode = True
