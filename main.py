8ee8df0149d657452ed522db9543bb1721cb56b1
rinkal159 <rinkalsingapuri@gmail.com>
2026-07-03 19:12:26 +0530

    Did few important things- tranformed from dictionary of blogs to postgres database, segregated all the backend routes in routers directory and wired up them in main.py file and written all the code of exception handling in separate exception handling file, just called it here.

--- main.py
+++ main.py
@@ -1,110 +1,71 @@
-from fastapi import FastAPI, Request, HTTPException, status
-from fastapi.exceptions import RequestValidationError
-from starlette.exceptions import HTTPException as StarletteException
-from fastapi.responses import JSONResponse
+from fastapi import FastAPI, Request, HTTPException, status, Depends
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 from datetime import datetime
+from exception_handling.exception_handlers import handlers
+from sqlalchemy.orm import Session
+from database import get_db
+from auth.authentication import get_current_user
 
-from schemas.blog_schema import BlogResponse as BlogResponseSchema
+from model import User, Blog
+
+from routers import auth_routers, blog_routers, user_routers
 
 app = FastAPI()
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
+app.mount("/media", StaticFiles(directory="media"), name="media")
 
 templates = Jinja2Templates(directory="templates")
 
 
-blogs = [
-    {
-        "id": 1,
-        "title": "First Blog of Rinkal",
-        "content": "Content of first blog",
-        "author": "Rinkal Singapuri",
-        "posted_at": datetime.now().date(),
-    },
-    {
-        "id": 2,
-        "title": "First Blog of Krina",
-        "content": "Content of first blog",
-        "author": "Krina Singapuri",
-        "posted_at": datetime.now().date(),
-    },
-]
-
-
 # * FRONT-END
+# get all the posts
 @app.get("/", include_in_schema=False, name="home")
 @app.get("/blogs", include_in_schema=False, name="blogs")
-def home(request: Request):
+def home(request: Request, db: Session = Depends(get_db)):
+    blogs = db.query(Blog).all()
     return templates.TemplateResponse(
         request, "index.html", {"blogs": blogs, "title": "Index"}
     )
 
 
+# get specific post
 @app.get("/blogs/{id}", include_in_schema=False)
-def blog(id: int, request: Request):
+def blog(id: int, request: Request, db: Session = Depends(get_db)):
+    blogs = db.query(Blog).all()
     for blog in blogs:
-        if blog["id"] == id:
-            return templates.TemplateResponse(request, "blog.html", {"blog": blog})
+        if blog.id == id:
+            return templates.TemplateResponse(
+                request, "blog.html", {"blog": blog, "title": blog.title[:50]}
+            )
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Blog not found")
+
+
+# get all blogs of a user
+@app.get("/blogs/individual/{id}")
+def get_individual_blogs(request: Request, id: int, db: Session = Depends(get_db)):
+    existed_user = db.query(User).filter(User.id == id).one_or_none()
+
+    if not existed_user:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Author not found"
+        )
+
+    blogs = db.query(Blog).filter(Blog.author_id == id).all()
+    name = blogs[0].author.name
+    return templates.TemplateResponse(
+        request,
+        "individual_blogs.html",
+        {"blogs": blogs, "name": name, "title": f"Posts by {name[:50]}"},
+    )
 
 
 # * BACK-END
-# ^ home
-@app.get("/api/blogs", response_model=list[BlogResponseSchema])
-def get_blogs_api():
-    return blogs
-
-
-# ^ blog
-@app.get("/api/blogs/{id}", response_model=BlogResponseSchema)
-def get_blog_api(id: int):
-    for blog in blogs:
-        if blog["id"] == id:
-            return blog
-    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Blog not found")
+app.include_router(auth_routers.auth_router)
+app.include_router(user_routers.user_router)
+app.include_router(blog_routers.blog_router)
 
 
 # * Exception handling
-@app.exception_handler(StarletteException)
-def exception_anything(request: Request, exception: StarletteException):
-    if request.url.path.startswith("/api"):
-        return JSONResponse(
-            status_code=exception.status_code, content={"error": exception.detail}
-        )
-
-    return templates.TemplateResponse(
-        request,
-        "error.html",
-        {"status_code": exception.status_code, "error_message": exception.detail},
-        status_code=exception.status_code
-    )
-
-
-@app.exception_handler(RequestValidationError)
-def exception_422(request: Request, exception: RequestValidationError):
-
-    if request.url.path.startswith("/api"):
-        errors = []
-
-        for error in exception.errors():
-            individual = {
-                "field": error.get("loc")[1],
-                "error_message": error.get("msg"),
-            }
-            errors.append(individual)
-
-        return JSONResponse(
-            status_code=status.HTTP_422_UNPROCESSABLE_CONTENT, content={"error": errors}
-        )
-
-    return templates.TemplateResponse(
-        request,
-        "error.html",
-        {
-            "status_code": status.HTTP_422_UNPROCESSABLE_CONTENT,
-            "error_message": "Invalid request. Please check your input and try again.",
-        },
-        status_code=status.HTTP_422_UNPROCESSABLE_CONTENT,
-    )
+handlers(app, templates)
