cdc4e24c83a8d1fe9fd5eedf142dc5e45f3b2c53
rinkal159 <rinkalsingapuri@gmail.com>
2026-07-03 19:13:22 +0530

    Adds indexing to primary keys and creates property of image_path to be accessible in User schemas.

--- model.py
+++ model.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine, String, DateTime, func, ForeignKey
 from sqlalchemy.orm import declarative_base, Mapped, mapped_column, relationship
-from .env_config import settings
+from env_config import settings
 from datetime import datetime
 
 engine = create_engine(settings.postgres_url)
@@ -11,11 +11,11 @@ Base = declarative_base()
 class User(Base):
     __tablename__ = "users"
 
-    id: Mapped[int] = mapped_column(primary_key=True)
+    id: Mapped[int] = mapped_column(primary_key=True, index=True)
     name: Mapped[str] = mapped_column(String(100))
-    email: Mapped[str] = mapped_column(String())
+    email: Mapped[str] = mapped_column(String(), unique=True)
     password: Mapped[str] = mapped_column(String())
-    profile_picture: Mapped[str] = mapped_column(String())
+    profile_picture: Mapped[str] = mapped_column(String(), nullable=True)
     bio: Mapped[str] = mapped_column(String(), nullable=True)
     created_at: Mapped[datetime] = mapped_column(
         DateTime(timezone=True), server_default=func.now()
@@ -24,12 +24,18 @@ class User(Base):
         DateTime(timezone=True), server_default=func.now()
     )
     blogs: Mapped[list["Blog"]] = relationship(back_populates="author")
+    
+    @property
+    def profile_picture_path(self):
+        if self.profile_picture:
+            return f"media/profic_pictures/{self.profile_picture}"
+        return "static/profile_pictures/default.jpg"
 
 
 class Blog(Base):
     __tablename__ = "blogs"
 
-    id: Mapped[int] = mapped_column(primary_key=True)
+    id: Mapped[int] = mapped_column(primary_key=True, index=True)
     title: Mapped[str] = mapped_column(String(100))
     content: Mapped[str] = mapped_column(String(1500))
     posted_at: Mapped[datetime] = mapped_column(
