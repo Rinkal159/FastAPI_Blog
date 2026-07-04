31560ae48ddd7d91ba64ac5ccae8ea813fd9f9f6
rinkal159 <rinkalsingapuri@gmail.com>
2026-07-04 22:01:26 +0530

    Creates routes for updating or deleting users.

--- routers/user_routers.py
+++ routers/user_routers.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends, status, HTTPException
+from fastapi import APIRouter, Depends, status, HTTPException, Response
 from sqlalchemy.orm import Session
 from database import get_db
 from utils.password_hashing import hash_password
@@ -6,7 +6,7 @@ from auth.authentication import get_current_user
 
 from model import User
 from schemas.user_schema import (
-    UserCreate as UserCreateSchema,
+    UserUpdate as UserUpdateSchema,
     UserResponse as UserResponseSchema,
 )
 
@@ -24,3 +24,27 @@ def get_user_api(
             status_code=status.HTTP_404_NOT_FOUND, detail="User not found"
         )
     return existed_user
+
+
+# * update user
+@user_router.patch("/", response_model=UserResponseSchema)
+def update_user(user: UserUpdateSchema, db: Session = Depends(get_db), current_user_id=Depends(get_current_user)):
+    existed_user = db.query(User).filter(User.id == current_user_id).one_or_none()
+
+    update_data = user.model_dump(exclude_unset=True)
+    for field, value in update_data.items():
+        setattr(existed_user, field, value)
+        
+    db.commit()
+    db.refresh(existed_user)
+    return existed_user
+
+
+# * delete user
+@user_router.delete("/", status_code=status.HTTP_204_NO_CONTENT)
+def delete_user(db: Session = Depends(get_db), current_user_id=Depends(get_current_user)):
+    existed_user = db.query(User).filter(User.id == current_user_id).one_or_none()
+    
+    db.delete(existed_user)
+    db.commit()
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
