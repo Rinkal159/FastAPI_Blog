4425c6613432b1b6dfab2c1860799c79fb790ee8
rinkal159 <rinkalsingapuri@gmail.com>
2026-07-07 23:06:32 +0530

    Gets the logged in user from the database.

---
+++ utils/user_In_response.py
@@ -0,0 +1,18 @@
+from fastapi import Request, Depends
+from auth.authentication import verify_token
+from sqlalchemy.ext.asyncio import AsyncSession
+from database import get_db
+from sqlalchemy import select
+from model import User
+
+async def user_in_response(request: Request, db: AsyncSession = Depends(get_db)):
+    token = request.cookies.get("access_token")
+    if not token:
+        return None
+    
+    user_id = verify_token(token)
+    user = None
+    if user_id:
+        user = await db.execute(select(User).where(User.id == user_id))
+        
+    return user
