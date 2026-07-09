72ad6210bd2f5ef72443f9b5e3d6be34557bcd70
rinkal159 <rinkalsingapuri@gmail.com>
2026-07-09 23:05:37 +0530

    Executes DELETE route for user.

---
+++ static/controllers/deleteUser.js
@@ -0,0 +1,41 @@
+import hideModal from "../js/hide_modal.js";
+import showModal from "../js/show_modal.js";
+
+const deleteBtn = document.getElementById("delete");
+
+deleteBtn.addEventListener("click", async () => {
+    try {
+        const response = await fetch(`/api/users/`, {
+            method: "DELETE"
+        })
+
+
+        if (response.ok) {
+            hideModal("DeleteUserModal")
+
+            document.getElementById("SuccessModalBody").textContent = `Your account has been deleted successfully!`
+            showModal("SuccessModal");
+
+            // redirects to '/' page when user closes the modal
+            document.getElementById("SuccessModal").addEventListener("hidden.bs.modal", () => {
+                window.location.href = '/'
+            }, { once: true })
+
+        } else {
+            const error = await response.json();
+            throw error.error;
+        }
+
+
+    } catch (e) {
+        if (Array.isArray(e)) {
+            document.getElementById("ErrorModalBody").innerHTML = `<ul>
+                ${e.map((val) => `<li><span class="fw-bold">${val.field}</span> : ${val.error_message}</li>`).join("")}
+            </ul>`
+        } else {
+            document.getElementById("ErrorModalBody").innerHTML = e;
+        }
+
+        showModal("ErrorModal");
+    }
+})
