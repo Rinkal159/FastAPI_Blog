b3886a3055c03c9564fb2a4ee31900b23f158a67
rinkal159 <rinkalsingapuri@gmail.com>
2026-07-09 23:04:53 +0530

    Builds route for changing password.

--- routers/user_routers.py
+++ routers/user_routers.py
@@ -4,11 +4,13 @@ from database import get_db
 from utils.password_hashing import hash_password
 from auth.authentication import get_current_user
 from sqlalchemy import select
+from utils.password_hashing import hash_password, verify_password
 
 from model import User
 from schemas.user_schema import (
     UserUpdate as UserUpdateSchema,
     UserResponse as UserResponseSchema,
+    UserPassword as UserPasswordSchema
 )
 
 user_router = APIRouter(prefix="/api/users", tags=["Users"])
@@ -58,3 +60,23 @@ async def delete_user_api(
     await db.delete(existed_user)
     await db.commit()
     return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+
+#* change password
+@user_router.patch("/password", response_model=UserResponseSchema)
+async def update_password_api(password: UserPasswordSchema, db: AsyncSession = Depends(get_db), current_user_id=Depends(get_current_user)):
+    result = await db.execute(select(User).where(User.id == current_user_id))
+    existed_user = result.scalars().one_or_none()
+    
+    if not existed_user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
+    
+    if not verify_password(password.current_password, existed_user.password):
+        raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail="Current password is incorrect")
+    
+    existed_user.password = hash_password(password.new_password)
+    await db.commit()
+    await db.refresh(existed_user)
+    return existed_user
+    
+    
